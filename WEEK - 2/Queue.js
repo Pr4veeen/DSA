04551900ce6827b617a4595790a966d52084a059
Pr4veenn <praveen7.info@gmail.com>
2025-04-08 13:32:10 +0530

    week 2 practice

--- WEEK - 2/Queue.js
+++ WEEK - 2/Queue.js
@@ -133,6 +133,93 @@ console.log(queueStack.dequeue())
 console.log(queueStack.peek())
 
 
+// // CREATE STACK USING QUEUE (2 QUEUE)
+
+class StackUsingQue {
+    constructor(){
+        this.queue1 = []
+        this.queue2 = []
+    }
+
+    push(value){
+        this.queue1.push(value)
+    }
+
+    pop(){
+        while(this.queue1.length > 1){
+            this.queue2.push(this.queue1.shift());
+        }
+
+        let popped = this.queue1.shift();
+        [this.queue1, this.queue2] = [this.queue2, this.queue1];
+
+        return popped;
+    }
+
+
+    display(){
+        console.log([...this.queue1])
+    }
+}
+
+//const stack = new StackUsingQue()
+
+stack.push(10)
+stack.push(20)
+stack.push(30)
+stack.push(40)
+stack.display()
+
+console.log(stack.pop())
+stack.display()
+
+//////////////
+
+// STACK USING QUEUE (1 QUEUE)
+
+class StackUsingQueue {
+    constructor(){
+        this.que = []
+    }
+    
+    push(value){
+        
+        this.que.push(value)
+        
+        let size = this.que.length
+        
+        while(size > 1){
+            this.que.push(this.que.shift())
+            size--
+        }
+    }
+    
+    pop(){
+        return this.que.shift()
+    }
+    
+    peek(){
+        return this.que[0]
+    }
+    
+    display(){
+        console.log(this.que)
+    }
+}
+
+const stack = new StackUsingQueue()
+
+stack.push(10)
+stack.push(20)
+stack.push(30)
+stack.push(40)
+stack.display()
+
+console.log(stack.pop())
+console.log(stack.peek())
+stack.display()
+
+
 //////////////////////
 
 // Queue using an Array
---
+++ WEEK - 2/string.js
@@ -0,0 +1,5 @@
+let word = "apple"
+
+let ans = word.split('').map((char, index) => char.repeat(index + 1)).join("-")
+
+console.log(ans)
