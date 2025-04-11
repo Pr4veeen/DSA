21621f244d3873d11ec5cbcc81a3462c4a3185f1
Pr4veenn <praveen7.info@gmail.com>
2025-04-11 23:11:18 +0530

    practice

--- WEEK - 2/CircularQueue.js
+++ WEEK - 2/CircularQueue.js
@@ -1,85 +1,85 @@
 
-// class CircularQue {
-//     constructor(capacity){
-//         this.items = new Array(capacity)
-//         this.capacity = capacity
-//         this.currentLength = 0
-//         this.rear = -1
-//         this.front = -1
-//     }
+class CircularQue {
+    constructor(capacity){
+        this.items = new Array(capacity)
+        this.capacity = capacity
+        this.currentLength = 0
+        this.rear = -1
+        this.front = -1
+    }
 
-//     isFull(){
-//         return this.currentLength === this.capacity
-//     }
+    isFull(){
+        return this.currentLength === this.capacity
+    }
 
-//     isEmpty(){
-//         return this.currentLength === 0
-//     }
+    isEmpty(){
+        return this.currentLength === 0
+    }
 
-//     enqueue(element){
-//         if(!this.isFull()){
-//             this.rear = (this.rear + 1) % this.capacity
-//             this.items[this.rear] = element
-//             this.currentLength += 1
-//             if(this.front === -1){
-//                 this.front = this.rear
-//             }
-//         }
-//     }
+    enqueue(element){
+        if(!this.isFull()){
+            this.rear = (this.rear + 1) % this.capacity
+            this.items[this.rear] = element
+            this.currentLength += 1
+            if(this.front === -1){
+                this.front = this.rear
+            }
+        }
+    }
 
-//     dequeue(){
-//         if(this.isEmpty()){
-//             return null
-//         }
-//         const item = this.items[this.front]
-//         this.items[this.front] = null
-//         this.front = (this.front + 1) % this.capacity
-//         this.currentLength -= 1
-//         if(this.isEmpty()){
-//             this.front = -1
-//             this.rear = -1
-//         }
-//         return item
-//     }
+    dequeue(){
+        if(this.isEmpty()){
+            return null
+        }
+        const item = this.items[this.front]
+        this.items[this.front] = null
+        this.front = (this.front + 1) % this.capacity
+        this.currentLength -= 1
+        if(this.isEmpty()){
+            this.front = -1
+            this.rear = -1
+        }
+        return item
+    }
 
-//     peek(){
-//         if(!this.isEmpty()){
-//             return this.items[this.front]
-//         }
-//         return null
-//     }
+    peek(){
+        if(!this.isEmpty()){
+            return this.items[this.front]
+        }
+        return null
+    }
 
-//     print(){
-//         if(this.isEmpty()){
-//             return null
-//         }else{
-//             let i
-//             let str =""
-//             for(i = this.front ; i !== this.rear ; i = (i+1) % this.capacity){
-//                 str += this.items[i] + " "
-//             }
-//             str += this.items[i]
-//             console.log(str)
-//         }
-//     }
-// }
+    print(){
+        if(this.isEmpty()){
+            return null
+        }else{
+            let i
+            let str =""
+            for(i = this.front ; i !== this.rear ; i = (i+1) % this.capacity){
+                str += this.items[i] + " "
+            }
+            str += this.items[i]
+            console.log(str)
+        }
+    }
+}
 
 
-// const queue = new CircularQue(5)
+const queue = new CircularQue(5)
 
-// queue.enqueue(10)
-// queue.enqueue(20)
-// queue.enqueue(30)
-// queue.enqueue(40)
-// queue.print()
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+queue.enqueue(40)
+queue.print()
 
-// console.log(queue.currentLength)
+console.log(queue.currentLength)
 
-// console.log(queue.dequeue())
-// console.log(queue.dequeue())
-// queue.print()
+console.log(queue.dequeue())
+console.log(queue.dequeue())
+queue.print()
 
-// queue.enqueue(60)
-// queue.print()
-// console.log(queue.currentLength)
+queue.enqueue(60)
+queue.print()
+console.log(queue.currentLength)
 
--- WEEK - 2/array.js
+++ WEEK - 2/array.js
@@ -1,34 +1,34 @@
 
-// // fIND UNCOMMON ELEMENTS FROM TWO DEFFERENT ARRAYS USING HASH SET
+// fIND UNCOMMON ELEMENTS FROM TWO DEFFERENT ARRAYS USING HASH SET
 
-// function Uncommon(arr1,arr2){
+function Uncommon(arr1,arr2){
 
-//     let hash = {}
+    let hash = {}
 
-//     for(let val of arr1){
-//         hash[val] = (hash[val] || 0) + 1
-//     }
+    for(let val of arr1){
+        hash[val] = (hash[val] || 0) + 1
+    }
 
-//     for(let val of arr2){
-//         if(hash[val]){
-//             hash[val] +=1
-//         }else{
-//             hash[val] = 1
-//         }
-//     }
+    for(let val of arr2){
+        if(hash[val]){
+            hash[val] +=1
+        }else{
+            hash[val] = 1
+        }
+    }
 
-//     let result = []
+    let result = []
 
-//     for(let key in hash){
-//         if(hash[key] === 1){
-//             result.push(key)
-//         }
-//     }
+    for(let key in hash){
+        if(hash[key] === 1){
+            result.push(key)
+        }
+    }
 
-//     return result
-// }
+    return result
+}
 
-// let arr1 = [1,2,3]
-// let arr2 = [3,4,5]
-// console.log(Uncommon(arr1,arr2))
+let arr1 = [1,2,3]
+let arr2 = [3,4,5]
+console.log(Uncommon(arr1,arr2))
 
